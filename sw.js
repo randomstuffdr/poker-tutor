f7f301b53c84622f6619d4b0f290d42419acc623
poker-tutor <noreply@poker-tutor.local>
2026-08-23 14:56:19 +0000

    Rediseño visual: magenta metálico sobre negro violáceo

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const V = "chuleta-09212b1e5f";
+const V = "pokertutor-22d662d2f5";
 const ASSETS = ["./","./index.html","./manifest.webmanifest",
   "./icon-192.png","./icon-512.png","./icon-maskable-512.png","./apple-touch-icon.png"];
 
