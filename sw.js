6061ec0a4da20f2490e985f5b31866e047fd799f
poker-tutor <noreply@poker-tutor.local>
2026-08-28 17:48:30 +0000

    Poker Tutor v7 (mejora incremental de Mega Hit)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const V = "pokertutor-197b2a42d7";
+const V = "pokertutor-0e36bc352c";
 const ASSETS = ["./","./index.html","./manifest.webmanifest",
   "./icon-192.png","./icon-512.png","./icon-maskable-512.png","./apple-touch-icon.png"];
 
